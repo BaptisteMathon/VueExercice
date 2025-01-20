cc7aa2874b3d2a3403d38010666392eae7505242
BaptisteMathon <103061783+BaptisteMathon@users.noreply.github.com>
2025-01-20 14:42:18 +0100

    exercice 1

---
+++ src/views/ProductList.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import {ref} from 'vue'
+export default {
+  name: 'ProductList',
+  setup(){
+    const ListProducts = ref([
+      {id: 1, label: 'Carotte', description: "de belles carottes", statut: "disponible", quantity: 5},
+      {id: 2, label: 'Pomme de terre', description: "de belles pomme de terre", statut: "disponible", quantity: 0},
+      {id: 3, label: 'Pomme', description: "de belle pomme", statut: "disponible", quantity: 15},
+    ])
+
+    ListProducts.value.forEach(element => {
+      element.statut = element.quantity === 0 ? "indisponible" : "disponible"
+    });
+
+    return {
+      ListProducts,
+
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in ListProducts">
+        {{ product.label }} : {{ product.description }} ( {{ product.statut }} )
+      </li>
+    </ul>
+  </div>
+</template>
